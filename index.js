ce4a590afe8e658ede60347b8bf17234dfa590e1
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-10-14 12:32:36 -0500

    Using defaults.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import ConsoleTyper from './dist/console-typer.js';
 
 const consoleTyper = new ConsoleTyper({
-  paragraphElement: document.getElementsByClassName('intro-paragraph')[0],
-  loop: false,
-  typingSpeedMs: 40,
-  stopCursorAfterBlinks: 200
+  paragraphElement: document.getElementsByClassName('intro-paragraph')[0]
 });
 
 consoleTyper.startTyping();
