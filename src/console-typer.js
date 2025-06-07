56a0535753385a7c5f3e36b17b3559571766d3ec
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2025-06-07 17:39:34 -0600

    updating all

--- src/console-typer.js
+++ src/console-typer.js
@@ -1,147 +1,199 @@
 export default class ConsoleTyper {
   constructor(props) {
-    const { paragraphElement } = props;
+    this.libName = "ConsoleTyper";
+    this.errorMessages = {
+      noParagraphArray: `${this.libName}: You must provide an array with at least one valid <p> element with text`,
+      notParagraph: `${this.libName}: Please provide a valid paragraph element`,
+      notContent: `${this.libName}: Please provide a paragraph with text`
+    };
 
-    if (!paragraphElement) {
-      console.error(this.errorMessages.notParagraph);
+    // Always expects an array of <p> elements
+    let paragraphElements = Array.isArray(props.paragraphElements) ? props.paragraphElements : [];
+    paragraphElements = paragraphElements.filter((p) => (p.innerHTML && p.innerHTML.length > 0));
+
+    if (!paragraphElements.length) {
+      console.error(this.errorMessages.noParagraphArray);
       return;
     }
 
-    if (paragraphElement.tagName !== "P") {
-      console.error(this.errorMessages.notParagraph);
-      return;
-    }
-
-    const textToType = paragraphElement.innerHTML;
-    if (textToType.length === 0) {
-      console.error(this.errorMessages.notContent);
-    }
-
+    const textsToType = paragraphElements.map((p) => p.innerHTML);
 
     this.defaultSettings = {
       loop: true,
-      loopAfterSeconds: 6,
+      loopAfterSeconds: 0.5,
       cursor: "I",
-      stopCursorAfterBlinks: 10,
+      stopCursorAfterBlinks: 1,
       cursorAnimationSpeedMs: 500,
-      typingSpeedMs: 70,
+      typingSpeedMs: 40,
       onStartTyping: () => {},
       onStopTyping: () => {},
       onStopCursorAnimation: () => {}
     };
-  
+
     this.state = {
-      isTyping: false
-    };
-  
-    this.libName = "ConsoleTyper";
-  
-    this.errorMessages = {
-      notParagraph: `${this.libName}: please provide a valid paragraph element`,
-      notContent: `${this.libName}: please provide a paragraph with text`
+      isTyping: false,
+      currentParagraphIndex: 0
     };
 
     this.settings = Object.assign(
       { ...this.defaultSettings },
       { ...props },
-      { textToType }
+      { paragraphElements, textsToType }
     );
-
-    this.cleanParagraph();
 
     this.type = this.type.bind(this);
     this.animateCursor = this.animateCursor.bind(this);
+    this.startTyping = this.startTyping.bind(this);
+    this.typeParagraph = this.typeParagraph.bind(this);
+    this.cleanAllParagraphs = this.cleanAllParagraphs.bind(this);
   }
 
-  cleanParagraph() {
-    this.settings.paragraphElement.innerHTML = " ";
+  cleanAllParagraphs() {
+    this.settings.paragraphElements.forEach((p) => {
+      p.innerHTML = " ";
+      const computed = window.getComputedStyle(p);
+      p.style.minHeight = computed.lineHeight;
+    });
   }
 
   onStopCursorAnimation() {
-    const { onStopCursorAnimation } = this.settings;
-    onStopCursorAnimation();
+    this.settings.onStopCursorAnimation();
   }
 
   onStartTyping() {
-    const { onStartTyping } = this.settings;
     this.state.isTyping = true;
-    onStartTyping();
+    this.settings.onStartTyping();
   }
 
   onStopTyping() {
-    const { onStopTyping } = this.settings;
     this.state.isTyping = false;
-    onStopTyping();
+    this.settings.onStopTyping();
   }
 
-  animateCursor(actualBlink) {
-    const {
-      paragraphElement,
-      cursor,
-      stopCursorAfterBlinks,
-      cursorAnimationSpeedMs
-    } = this.settings;
+  animateCursor(actualBlink, paragraphElement, options = {}) {
+    const { cursor, stopCursorAfterBlinks, cursorAnimationSpeedMs } = this.settings;
+    const { initial = false, onFinish = null, blinks = 2 } = options;
 
-    const actualText = paragraphElement.innerHTML.split("");
-    const lastChar = actualText.pop();
     let newText = "";
-
-    if (lastChar === cursor) {
-      newText = actualText.join("");
+    if (initial) {
+      newText = actualBlink % 2 === 0 ? cursor : "";
     } else {
-      if (actualBlink >= stopCursorAfterBlinks) {
-        this.onStopCursorAnimation();
-        return;
+      const actualText = paragraphElement.innerHTML;
+      if (actualText.endsWith(cursor)) {
+        newText = actualText.slice(0, -cursor.length);
+      } else {
+        if (actualBlink >= blinks) {
+          this.onStopCursorAnimation();
+          if (onFinish) onFinish();
+          return;
+        }
+        newText = paragraphElement.innerHTML + cursor;
       }
-      newText = paragraphElement.innerHTML + cursor;
     }
 
     setTimeout(() => {
-      if (this.state.isTyping === true) {
+      if (this.state.isTyping === true && !initial) {
         this.onStopCursorAnimation();
+        if (onFinish) onFinish();
         return;
       }
       paragraphElement.innerHTML = newText;
-      this.animateCursor(actualBlink + 1);
+      if (initial) {
+        if (actualBlink >= blinks * 2 - 1) {
+          if (onFinish) onFinish();
+          return;
+        }
+        this.animateCursor(actualBlink + 1, paragraphElement, { initial: true, onFinish, blinks });
+      } else {
+        this.animateCursor(actualBlink + 1, paragraphElement, { onFinish, blinks });
+      }
     }, cursorAnimationSpeedMs);
   }
 
-  type(textArray) {
+  type(textArray, paragraphElement, onFinish, isLastParagraph = false) {
     if (textArray.length <= 0) {
+      if (onFinish) onFinish();
       return;
     }
 
-    const { paragraphElement, cursor, loop, typingSpeedMs } = this.settings;
+    const { cursor, typingSpeedMs } = this.settings;
 
-    let actualText = paragraphElement.innerHTML.split("");
+    let actualText = paragraphElement.innerHTML;
+    if (actualText.endsWith(cursor)) {
+      actualText = actualText.slice(0, -cursor.length);
+    }
     const newChar = textArray.shift();
 
-    actualText.pop();
-    actualText = actualText.join("");
-
-    this.settings.paragraphElement.innerHTML = actualText + newChar + cursor;
+    paragraphElement.innerHTML = actualText + newChar + cursor;
 
     if (textArray.length > 0) {
-      setTimeout(() => this.type(textArray), typingSpeedMs);
+      setTimeout(() => this.type(textArray, paragraphElement, onFinish, isLastParagraph), typingSpeedMs);
     } else {
-      if (loop) {
-        this.programLoop();
-      }
-
       this.onStopTyping();
-      this.animateCursor(0);
+      if (isLastParagraph) {
+        this.animateCursor(0, paragraphElement, { onFinish, blinks: 2 });
+      } else {
+        if (paragraphElement.innerHTML.endsWith(cursor)) {
+          paragraphElement.innerHTML = paragraphElement.innerHTML.slice(0, -cursor.length);
+        }
+        if (onFinish) onFinish();
+      }
+    }
+  }
+
+  typeParagraph(index) {
+    const { paragraphElements, textsToType } = this.settings;
+    if (index >= paragraphElements.length) {
+      if (this.settings.loop) {
+        this.animateCursor(0, paragraphElements[paragraphElements.length - 1], {
+          onFinish: () => {
+            setTimeout(() => {
+              this.cleanAllParagraphs();
+              this.programLoop();
+            }, this.settings.loopAfterSeconds * 1000);
+          },
+          blinks: 2
+        });
+      }
+      return;
+    }
+    this.state.currentParagraphIndex = index;
+    if (index === 0) {
+      this.cleanAllParagraphs();
+      this.onStartTyping();
+      this.animateCursor(0, paragraphElements[index], {
+        initial: true,
+        onFinish: () => {
+          paragraphElements[index].innerHTML = "";
+          this.type(
+            textsToType[index].split(""),
+            paragraphElements[index],
+            () => {
+              this.typeParagraph(index + 1);
+            },
+            paragraphElements.length === 1
+          );
+        },
+        blinks: 2
+      });
+    } else {
+      this.onStartTyping();
+      this.type(
+        textsToType[index].split(""),
+        paragraphElements[index],
+        () => {
+          this.typeParagraph(index + 1);
+        },
+        index === paragraphElements.length - 1
+      );
     }
   }
 
   programLoop() {
-    const { loopAfterSeconds } = this.settings;
-    setTimeout(() => this.startTyping(), loopAfterSeconds * 1000);
+    this.startTyping();
   }
 
   startTyping() {
-    const { textToType } = this.settings;
-    this.onStartTyping();
-    this.cleanParagraph();
-    this.type(textToType.split(""));
+    this.typeParagraph(0);
   }
 }
